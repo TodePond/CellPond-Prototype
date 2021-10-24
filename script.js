d352feae0992e18cbff253b439c6aa450fe0158f
Luke Wilson <l2wilson94@gmail.com>
2021-10-24 12:21:22 +0100

    zoooom

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const context = canvas.getContext("2d")
 canvas.style["background-color"] = Colour.Grey
 canvas.style["margin"] = 0
 
-let c = {}
 on.load(async () => {
 
 	document.body.style["background-color"] = Colour.Black
@@ -14,9 +13,7 @@ on.load(async () => {
 	document.body.style["overflow"] = "hidden"
 	document.body.appendChild(canvas)
 
-	c = await loadWasm("script.wasm", environment)
-	c.setup()
-	setInterval(tick, 1000 / 2)
+	setInterval(tick, 1000 / 60)
 
 	trigger("resize")
 })
@@ -29,7 +26,6 @@ on.resize(() => {
 	canvas.height = document.body.clientHeight
 	canvas.style["width"] = canvas.width
 	canvas.style["height"] = canvas.height
-	c.resize(document.body.clientWidth, document.body.clientHeight)
 	draw()
 })
 
@@ -40,9 +36,104 @@ on.keydown(e => {
 	}
 })
 
+const camera = {
+	x: 0,
+	y: 0,
+	scale: 1.0,
+}
+
+on.mousemove(e => {
+	if (Mouse.Right) {
+		camera.x += e.movementX
+		camera.y += e.movementY
+		updateDropperPosition()
+		draw()
+	}
+})
+
+const CAMERA_ZOOM_SPEED = 0.05
+on.mousewheel(e => {
+	if (e.deltaY > 0) {
+		const zoom = (camera.scale - camera.scale * (1 - CAMERA_ZOOM_SPEED))
+		camera.scale -= zoom
+		
+		//camera.x += zoom * Mouse.position[0]
+		//camera.y += zoom * Mouse.position[1]
+
+		camera.x += zoom * dropper.x
+		camera.y += zoom * dropper.y
+
+		//camera.x += 0.1 * (Mouse.position[0] - canvas.width/2) * 1.5
+		//camera.y += 0.1 * (Mouse.position[1] - canvas.height/2) * 1.5
+	}
+	else {
+		const zoom = (camera.scale - camera.scale * (1 - CAMERA_ZOOM_SPEED))
+		camera.scale += zoom
+		
+		//const xRatio = canvas.width / CELL_SIZE
+
+		camera.x -= zoom * dropper.x
+		camera.y -= zoom * dropper.y
+		//camera.y -= 0.1 * (Mouse.position[1] - canvas.height/2) * 1.5
+		
+	}
+	updateDropperPosition()
+	draw()
+})
+
+on.contextmenu(e => {
+	e.preventDefault()
+})
+
+//=================//
+// DRAWING USEFULS //
+//=================//
+const REDS   = [23, 55, 70,  98, 128, 159, 174, 204, 242, 255]
+const GREENS = [29, 67, 98, 128, 159, 174, 204, 222, 245, 255]
+const BLUES  = [40, 70, 98, 128, 159, 174, 201, 222, 247, 255]
+
+const COLOUR_BLACK = [0, 0, 0]
+const COLOUR_GREY = [1, 1, 2]
+const COLOUR_SILVER = [5, 5, 6]
+const COLOUR_WHITE = [9, 9, 9]
+const COLOUR_GREEN = [2, 9, 3]
+const COLOUR_RED = [9, 1, 1]
+const COLOUR_BLUE = [2, 3, 9]
+const COLOUR_YELLOW = [9, 6, 1]
+const COLOUR_ORANGE = [9, 3, 1]
+const COLOUR_PINK = [9, 3, 4]
+const COLOUR_CYAN = [2, 6, 9]
+const COLOUR_PURPLE = [4, 1, 9]
+
+const makeColourStyle = ([R, G, B]) => {
+	const r = REDS[R]
+	const g = GREENS[G]
+	const b = BLUES[B]
+	const style = "rgb(" + r + "," + g + "," + b + ")"
+	return style
+}
+
+//============//
+// WORLD DATA //
+//============//
+const makeCell = (content = [0, 0, 0], isMulti = false) => {
+	const cell = {
+		isMulti,
+		content,
+	}
+	return cell
+}
+
+const world = makeCell([0, 0, 0], false)
+
 //===========//
 // GAME LOOP //
 //===========//
+let dropper = {
+	x: undefined,
+	y: undefined,
+}
+
 const tick = () => {
 	if (!paused) {
 		update()
@@ -51,9 +142,50 @@ const tick = () => {
 }
 
 const update = () => {
-	c.update()
+	updateDropperPosition()
+}
+
+const updateDropperPosition = () => {
+	const [dx, dy] = getDropperPosition()
+	dropper.x = dx
+	dropper.y = dy
+}
+
+const getDropperPosition = () => {
+	const [mx, my] = Mouse.position
+	const [sx, sy] = [(mx-camera.x) / camera.scale, (my-camera.y) / camera.scale]
+	return [sx, sy]
 }
 
 const draw = () => {
-	c.draw()
+
+	context.clearRect(0, 0, canvas.width, canvas.height)
+
+	context.translate(camera.x, camera.y)
+
+	//context.translate(canvas.width/2, canvas.height/2)
+	context.scale(camera.scale, camera.scale)
+	//context.translate(-canvas.width/2, -canvas.height/2)
+
+	drawCell(world)
+	drawDropper()
+
+	//context.translate(canvas.width/2, canvas.height/2)
+	context.scale(1/camera.scale, 1/camera.scale)
+	//context.translate(-canvas.width/2, -canvas.height/2)
+
+	context.translate(-camera.x, -camera.y)
+}
+
+const CELL_SIZE = 1000
+const drawCell = (cell) => {
+	const style = makeColourStyle(cell.content)
+	context.fillStyle = style
+	context.fillRect(0, 0, CELL_SIZE, CELL_SIZE)
+}
+
+const DROPPER_SIZE = 50
+const drawDropper = () => {
+	context.strokeStyle = "white"
+	context.strokeRect(dropper.x - DROPPER_SIZE/2, dropper.y - DROPPER_SIZE/2, DROPPER_SIZE, DROPPER_SIZE)
 }
